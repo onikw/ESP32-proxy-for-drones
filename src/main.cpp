11927a264bb94d3ca7865755516f8b200716b98d
Bartosz Bartoszewski <bartosz.bartoszewski@reakto.eu>
2024-09-09 21:57:19 +0200

    add proxy - to check

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,20 @@
 #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
-
-
 #include <HardwareSerial.h>
 #include <MAVLink.h>
-
+#include <sbus.h>
 
 void sendMission(float lat_wp, float lon_wp, float altitude, float delay_seconds);
+boolean fetch_and_read(bfs::SbusRx& sbus_rx);
+void send(bfs::SbusTx& sbus_tx);
+
 uint8_t system_id = 1;
 uint8_t component_id = MAV_COMP_ID_AUTOPILOT1;
 uint8_t target_system = 1;       // ID Twojego drona
 uint8_t target_component = MAV_COMP_ID_AUTOPILOT1;
 
-HardwareSerial mySerial(1);
-
-// uint8_t system_id = 1;
-// uint8_t component_id = 200;
-// uint8_t target_system = 1;
-// uint8_t target_component = 1;
+HardwareSerial mySerial(1);  // Używamy portu szeregowego 1
 
 const char* ssid = "toya89029830";
 const char* password = "21378317";
@@ -35,38 +31,68 @@ WebServer server(80);
 double longitude = 0.0;
 double latitude = 0.0;
 double altitude = 0.0;
-bool dataReceived = false; 
+double delay_seconds = 300.0;
 
+bool dataReceived = false; 
 bool setMavlink = false; 
+bool set_proxy = false;
+
+// dane do sbus
+const int RX_PIN = 4;
+const int TX_PIN = 5;
+const int SW_PIN = 20;
+
+// throttle
+const int THR_CH = 2;
+const int THR_0 = 200;
+const int THR_60 = 1200;
+
+// arm
+const int ARM_CH = 4;
+const int ARM_FALSE = 200;
+const int ARM_TRUE = 1800;
+
+// mode
+const int MODE_CH = 5;
+const int MODE_1 = 200;
+const int MODE_2 = 1000;
+const int MODE_3 = 1800;
+
+// czas spadku [ms]
+const int TIME_FALL = 500;
+const int TIME_60 = 4500;
+const int REPEAT = 20;
+
+int thr_val = THR_0;
+int arm_val = ARM_FALSE;
+int mode_val = MODE_1;
+
+bfs::SbusData data;
+boolean is_mounted = false;
+boolean is_released = true;
+boolean is_auto = false;
 
 void setup() {
   delay(2000);
   Serial.begin(115200);
 
-  // Ustawienie statycznego IP
+  // Konfiguracja Wi-Fi
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("STA Failed to configure");
   }
-
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to Wi-Fi");
-
   while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
   }
-
   Serial.println("\nConnected to Wi-Fi");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
 
   // Endpoint do ustawiania współrzędnych
-server.on("/set-coordinates", HTTP_POST, []() {
+  server.on("/set-coordinates", HTTP_POST, []() {
     if (server.hasArg("plain")) {
       String body = server.arg("plain");
-
       StaticJsonDocument<200> doc;
       DeserializationError error = deserializeJson(doc, body);
-
       if (error) {
         server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"Invalid JSON\"}");
         return;
@@ -75,68 +101,118 @@ server.on("/set-coordinates", HTTP_POST, []() {
       longitude = doc["long"];
       latitude = doc["lat"];
       altitude = doc["altitude"];
+      delay_seconds = doc["delay"];
 
-      // Wysyłanie odpowiedzi do klienta przed zamknięciem serwera
       server.send(200, "application/json", "{\"status\":\"success\"}");
-      
-      // Wypisywanie otrzymanych danych do konsoli
-      Serial.println("Received data:");
-      Serial.print("Longitude: ");
-      Serial.println(longitude);
-      Serial.print("Latitude: ");
-      Serial.println(latitude);
-      Serial.print("Altitude: ");
-      Serial.println(altitude);
-
-      // Opóźnienie, aby klient miał czas na otrzymanie odpowiedzi
-      delay(1000); 
-
-      // Zamknięcie serwera i połączenia Wi-Fi
-      server.stop();
-      WiFi.disconnect(true);
-      Serial.println("Server stopped and Wi-Fi disconnected.");
       dataReceived = true;
 
-      
+      // Zatrzymanie serwera po odebraniu danych
+      server.stop();
+      WiFi.disconnect(true);
     } else {
       server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"No data received\"}");
     }
-});
-
+  });
   server.begin();
-  Serial.println("HTTP server started");
 }
 
-
 void loop() {
-  mavlink_message_t msg;
-  mavlink_status_t status;
-
-  // Obsługa klienta
   server.handleClient();
 
-  // Sprawdzanie, czy dane zostały odebrane
   if (dataReceived) {
-    
-    // Inicjalizacja MAVLink na pierwsze uruchomienie
-    if (setMavlink == false) {
-      mySerial.begin(57600, SERIAL_8N1, 2, 3);  // Inicjalizacja komunikacji szeregowej
-      Serial.println("Połączono z FC przez port szeregowy z użyciem MAVLink");
+    if (!setMavlink) {
+      mySerial.begin(57600, SERIAL_8N1, 2, 3);  // Komunikacja MAVLink na pinach 2 i 3
       setMavlink = true;
-      sendMission(float(latitude), float(longitude), float(altitude), 10.0);
+      sendMission(latitude, longitude, altitude, delay_seconds);
     }
 
     while (mySerial.available()) {
-      uint8_t c = mySerial.read();  // Odczyt bajtu danych
+      mavlink_message_t msg;
+      mavlink_status_t status;
+      uint8_t c = mySerial.read();
       if (mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
         Serial.println("Odebrano wiadomość MAVLink");
+        mySerial.end();  // Zakończenie komunikacji MAVLink
+        Serial.println("Zakończono komunikację MAVLink");
+
+        // Przełączenie na SBUS po zakończeniu komunikacji MAVLink
+        set_proxy = true;
+        dataReceived = false;
+        break;
+      }
+    }
+
+    if (set_proxy) {
+      // Inicjalizacja SBUS na tych samych pinach co wcześniej MAVLink
+      mySerial.begin(100000, SERIAL_8E2, RX_PIN, TX_PIN);  // Przełączenie na piny SBUS
+      bfs::SbusRx sbus_rx(&mySerial, RX_PIN, TX_PIN, true);
+      bfs::SbusTx sbus_tx(&mySerial, RX_PIN, TX_PIN, true);
+      
+      // Logika SBUS
+      if (is_released && digitalRead(SW_PIN) == LOW) {
+        is_mounted = true;
+        is_released = false;
+      }
+      if (!is_released && digitalRead(SW_PIN) == HIGH) {
+        is_auto = true;
+        is_mounted = false;
+        thr_val = THR_0;
+        arm_val = ARM_TRUE;
+        mode_val = MODE_2;
+
+        for (int i = 0; i < TIME_FALL; i += REPEAT) {
+          fetch_and_read(sbus_rx);
+          send(sbus_tx);
+          delay(REPEAT);
+        }
+
+        thr_val = THR_60;
+        for (int i = 0; i < TIME_60; i += REPEAT) {
+          fetch_and_read(sbus_rx);
+          send(sbus_tx);
+          delay(REPEAT);
+        }
+
+        fetch_and_read(sbus_rx);
+        mode_val = MODE_3;
+        send(sbus_tx);
+
+        is_released = true;
+      }
+      if (fetch_and_read(sbus_rx)) {
+        send(sbus_tx);
       }
     }
   }
 }
 
+boolean fetch_and_read(bfs::SbusRx& sbus_rx) {
+  if (sbus_rx.Read()) {
+    data = sbus_rx.data();
+    if (!is_mounted && data.ch[ARM_CH] >= ARM_TRUE - 100) {
+      is_auto = false;
+    }
+    return true;
+  }
+  return false;
+}
+
+void send(bfs::SbusTx& sbus_tx) {
+  if (is_auto) {
+    if (!is_released) {
+      data.ch[THR_CH] = thr_val;
+    }
+    data.ch[ARM_CH] = arm_val;
+    data.ch[MODE_CH] = mode_val;
+  }
+  sbus_tx.data(data);
+  sbus_tx.Write();
+}
+
+
 
 void sendMission(float lat_wp, float lon_wp, float altitude, float delay_seconds) {
+
   mavlink_message_t msg;
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
   uint16_t len;
